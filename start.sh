a1c70fbb06a24aa9964fd15146d06b0ce216fceb
Vadim Rutkovsky <roignac@gmail.com>
2018-11-28 11:05:20 +0100

    start.sh: add PODMAN_PULL and PODMAN_RUN vars

--- start.sh
+++ start.sh
@@ -10,6 +10,9 @@ USERNAME=$1
 INSTALLER_IMAGE="registry.svc.ci.openshift.org/openshift/origin-v4.0:installer"
 ANSIBLE_IMAGE="quay.io/vrutkovs/openshift-40-centos"
 PODMAN="sudo podman"
+PODMAN_PULL="${PODMAN} pull"
+PODMAN_RUN="${PODMAN} run"
+
 
 if [ ! -f ./pull_secret.json ]; then
   echo "Pull secret not found!"
@@ -26,12 +29,12 @@ rm -rvf install-config.yml .openshift_install_state.json .openshift_install.log 
 
 echo
 echo "Fetching installer"
-${PODMAN} pull ${INSTALLER_IMAGE}
-${PODMAN} run -rm -ti ${INSTALLER_IMAGE} version
+${PODMAN_PULL} ${INSTALLER_IMAGE}
+${PODMAN_RUN} -rm -ti ${INSTALLER_IMAGE} version
 
 echo
 echo "Creating bootstrap.ign"
-${PODMAN} run --rm \
+${PODMAN_RUN} --rm \
   -v $(pwd):/output \
   -e OPENSHIFT_INSTALL_PLATFORM="libvirt" \
   -e OPENSHIFT_INSTALL_LIBVIRT_URI="qemu+tcp://192.168.122.1/system" \
@@ -49,7 +52,7 @@ sed -i "/worker/{n;s/1/3/}" .openshift_install_state.json
 sed -i "/master/{n;n;s/1/3/}" install-config.yml
 sed -i "/worker/{n;n;s/1/3/}" install-config.yml
 
-${PODMAN} run --rm \
+${PODMAN_RUN} --rm \
   -v $(pwd):/output \
   -e OPENSHIFT_INSTALL_PLATFORM="libvirt" \
   -e OPENSHIFT_INSTALL_LIBVIRT_URI="qemu+tcp://192.168.122.1/system" \
@@ -67,8 +70,8 @@ echo
 echo "Provisioning GCP cluster"
 mkdir -p ./auth
 chmod 777 ./auth
-${PODMAN} pull ${ANSIBLE_IMAGE}
-${PODMAN} run --rm \
+${PODMAN_PULL} ${ANSIBLE_IMAGE}
+${PODMAN_RUN} --rm \
   -v $(pwd)/injected:/usr/share/ansible/openshift-ansible/inventory/dynamic/injected \
   -v $(pwd)/auth:/tmp/artifacts/installer/auth \
   -e INSTANCE_PREFIX="${USERNAME}" \
