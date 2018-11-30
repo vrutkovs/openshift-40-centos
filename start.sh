e470efade8614dcc5f0f31cae56771041af28aa8
Vadim Rutkovsky <roignac@gmail.com>
2018-11-30 11:31:42 +0100

    Customize selinux flags via MOUNT_FLAGS var and add comments to vars

--- start.sh
+++ start.sh
@@ -9,9 +9,15 @@ fi
 USERNAME=$1
 INSTALLER_IMAGE="registry.svc.ci.openshift.org/openshift/origin-v4.0:installer"
 ANSIBLE_IMAGE="quay.io/vrutkovs/openshift-40-centos"
+
+# Replace with empty string to skip selinux relabel
+MOUNT_FLAGS=":z"
+# Update with docker if podman is not available
 PODMAN="sudo podman"
 PODMAN_PULL="${PODMAN} pull"
-PODMAN_RUN="${PODMAN} run --rm -v $(pwd):/output:z --user $(id -u)"
+# Add necessary flags, e.g. '--net=host --privileged' if needed
+PODMAN_RUN="${PODMAN} run --rm -v $(pwd):/output${MOUNT_FLAGS} --user $(id -u)"
+# Adjust installer params
 PODMAN_PARAMS="-e OPENSHIFT_INSTALL_PLATFORM=libvirt \
 -e OPENSHIFT_INSTALL_LIBVIRT_URI=qemu+tcp://192.168.122.1/system \
 -e OPENSHIFT_INSTALL_LIBVIRT_IMAGE=file:///unused \
@@ -58,8 +64,8 @@ mkdir -p ./auth
 chmod 777 ./auth
 ${PODMAN_PULL} ${ANSIBLE_IMAGE}
 ${PODMAN_RUN} \
-  -v $(pwd)/injected:/usr/share/ansible/openshift-ansible/inventory/dynamic/injected:z \
-  -v $(pwd)/auth:/tmp/artifacts/installer/auth:z \
+  -v $(pwd)/injected:/usr/share/ansible/openshift-ansible/inventory/dynamic/injected${MOUNT_FLAGS} \
+  -v $(pwd)/auth:/tmp/artifacts/installer/auth${MOUNT_FLAGS} \
   -e INSTANCE_PREFIX="${USERNAME}" \
   -e OPTS="-vvv" \
   -ti ${ANSIBLE_IMAGE}
@@ -72,7 +78,7 @@ echo "Exit the shell to deprovision cluster"
 sh
 
 ${PODMAN_RUN} \
-  -v $(pwd)/injected:/usr/share/ansible/openshift-ansible/inventory/dynamic/injected:z \
+  -v $(pwd)/injected:/usr/share/ansible/openshift-ansible/inventory/dynamic/injected${MOUNT_FLAGS} \
   -e INSTANCE_PREFIX="${USERNAME}" \
   -e OPTS="-vvv" \
   -ti ${ANSIBLE_IMAGE} \
